62097816f0f64d448bcf07474c06663b69aae6fb
Dezvops <144513811+Dezvops@users.noreply.github.com>
2023-10-09 21:20:13 -0400

    Delete app.sh

--- app.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#Trying to push my first script to GitHub
-echo "Please enter your first name: "
-read name1
-echo "Please enter your last name: "
-read name2
-echo "You are welcome to Etech Consulting Masterclass $name1 $name2!"
